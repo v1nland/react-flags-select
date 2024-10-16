e28ff8e90e971ae32917cd115488faa8de8bb165
Martin Saavedra <martin.saavedra@smartside.cl>
2024-10-16 16:25:12 -0300

    feat: add build to src

---
+++ build/components/Flags/Countries/Intl.d.ts
@@ -0,0 +1,3 @@
+import * as React from "react";
+declare function SvgIntl(props: React.SVGProps<SVGSVGElement>): JSX.Element;
+export default SvgIntl;
---
+++ build/components/Flags/Countries/Ix.d.ts
@@ -0,0 +1,3 @@
+import * as React from "react";
+declare function SvgIx(props: React.SVGProps<SVGSVGElement>): JSX.Element;
+export default SvgIx;
